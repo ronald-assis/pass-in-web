28b9dcc08079e23bdea4388f87ee247b77ecaad1
ronald.assis <ronaldassis71@gmail.com>
2024-05-08 14:28:35 -0300

    feat: adding integration with base

--- src/components/attendee-list/index.tsx
+++ src/components/attendee-list/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import {
   Search,
   MoreHorizontal,
@@ -16,35 +16,69 @@ import { Table } from '../table'
 import { TableHeader } from '../table/table-header'
 import { TableCell } from '../table/table-cell'
 import { TableRow } from '../table/table-row'
-import { attendees } from '../../data/attendees'
 
 dayjs.extend(relativeTime)
 dayjs.locale('pt-br')
 
+interface Attendee {
+  id: string
+  name: string
+  email: string
+  createdAt: string
+  checkedInAt: string | null
+}
+
 export function AttendeeList() {
   const [search, setSearch] = useState('')
-  const [page, setPage] = useState(1)
+  // const [page, setPage] = useState(1)
+  const [attendees, setAttendees] = useState<Attendee[]>([])
+  const [total, setTotal] = useState(0)
+  const page = 1
 
-  const totalPages = Math.ceil(attendees.length / 10)
+  const totalPages = Math.ceil(total / 10)
+
+  useEffect(() => {
+    const url = new URL(
+      'http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees',
+    )
+    url.searchParams.set('pageIndex', String(page - 1))
+
+    if (search.length > 0) {
+      url.searchParams.set('query', search)
+    }
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setAttendees(data.attendees)
+        setTotal(data.total)
+      })
+  }, [page, search])
 
   const onSearchInputChanged = (event: ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value)
+    // setPage(1)
   }
 
   const goToNextPage = () => {
-    setPage(page + 1)
+    // setPage(page + 1)
+
+    const searchParams = new URLSearchParams(window.location.search)
+
+    searchParams.set('page', String(page + 1))
+    window.location.search = searchParams.toString()
   }
 
   const goToPreviousPage = () => {
-    setPage(page - 1)
+    // setPage(page - 1)
   }
 
   const goToFirstPage = () => {
-    setPage(1)
+    // setPage(1)
   }
 
   const goToLastPage = () => {
-    setPage(totalPages)
+    // setPage(totalPages)
   }
 
   return (
@@ -54,13 +88,12 @@ export function AttendeeList() {
         <div className="px-3 gap-3 py-1.5 border w-72 border-white/10 rounded-lg text-sm flex items-center">
           <Search className="size-4 text-emerald-300" />
           <input
-            className="bg-transparent flex-1 outline-none border-0 p-0 text-sm"
+            className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0"
             type="text"
             placeholder="Buscar participante..."
             onChange={onSearchInputChanged}
           />
         </div>
-        {search}
       </article>
 
       <Table>
@@ -69,7 +102,7 @@ export function AttendeeList() {
             <TableHeader style={{ width: 48 }}>
               <input
                 type="checkbox"
-                className="bg-black/20 rounded size-4 border border-white/10 checked:focus:bg-orange-400 checked:hover:bg-orange-400 checked:bg-orange-400"
+                className="bg-black/20 rounded size-4 border border-white/10 checked:focus:bg-orange-400 checked:hover:bg-orange-400 checked:bg-orange-400 focus:ring-0"
               />
             </TableHeader>
             <TableHeader>Código</TableHeader>
@@ -80,12 +113,12 @@ export function AttendeeList() {
           </tr>
         </thead>
         <tbody>
-          {attendees.slice((page - 1) * 10, page * 10).map((attendee) => (
+          {attendees.map((attendee) => (
             <TableRow key={attendee.id}>
               <TableCell>
                 <input
                   type="checkbox"
-                  className="bg-black/20 rounded size-4 border border-white/10 checked:focus:bg-orange-400 checked:hover:bg-orange-400 checked:bg-orange-400"
+                  className="bg-black/20 rounded size-4 border border-white/10 checked:focus:bg-orange-400 checked:hover:bg-orange-400 checked:bg-orange-400 focus:ring-0"
                 />
               </TableCell>
               <TableCell>{attendee.id}</TableCell>
@@ -98,7 +131,13 @@ export function AttendeeList() {
                 </div>
               </TableCell>
               <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
-              <TableCell>{dayjs().to(attendee.checkedInAt)}</TableCell>
+              <TableCell>
+                {attendee.checkedInAt === null ? (
+                  <span className="text-zinc-400">Não fez check-in</span>
+                ) : (
+                  dayjs().to(attendee.checkedInAt)
+                )}
+              </TableCell>
               <TableCell>
                 <IconButton transparent>
                   <MoreHorizontal className="size-4" />
@@ -109,7 +148,7 @@ export function AttendeeList() {
         </tbody>
         <tfoot>
           <TableCell colSpan={3}>
-            Mostrando 10 de {attendees.length} itens
+            Mostrando {attendees.length} de {total} itens
           </TableCell>
           <TableCell className="text-right" colSpan={3}>
             <div className="inline-flex items-center gap-8">
